5cda4c91a656da6b2aa3516575cff156e5193f0e
Chepkemboi <Chepkemboirabecca@gmail.com>
2023-02-02 12:02:07 +0300

    create file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var fullname = "rabecca hepkemboi"
+    var age = 20
+    var phoneNumber: Int = 716607726
+    var weighInKg = 56
+    val Kenyan: Boolean = true
+    var notkenyan: Boolean = false
+
+    println("age")
+
+
+}
